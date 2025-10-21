8d7c4723a4f0414733d10d92326999917a33a86b
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-10-21 20:51:31 -0300

    fix: welcome meesage

--- src/app/_components/WelcomeMessage.tsx
+++ src/app/_components/WelcomeMessage.tsx
@@ -1,18 +1,20 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { toast } from 'react-hot-toast'
 
 const WelcomeMessage = () => {
 	const { data: session } = useSession()
+	const [alreadyWelcomed, setAlreadyWelcomed] = useState(false)
 
 	useEffect(() => {
-		if (!session) return
+		if (!session || alreadyWelcomed) return
 
 		const { name } = session.user
 
 		toast(`¡Bienvenido, ${name}!`, { icon: '👏' })
+		setAlreadyWelcomed(true)
 	}, [session])
 
 	return null
